1c46650bec316b03c664f90280be554b6b8f2e03
djfannywang@gmail <djfannywang@gmail.com>
2021-08-26 14:09:58 +0300

    working road gen

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -15,6 +15,11 @@ public class RoadGenerator : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            UpdateChunk();
+        }
+        
         if (transformPlayer==null || previousChunk == null)
         {
             return;
@@ -30,15 +35,28 @@ public class RoadGenerator : MonoBehaviour
     {
         currentChunk = Instantiate(Resources.Load<RoadChunk>("Road/TestRoad/2"));
         var absoluteMovement = transformPlayer.position - currentChunk.start.position;
-        currentChunk.transform.position = absoluteMovement;//new Vector3(absoluteMovement.x, 0, absoluteMovement.z);
+        currentChunk.transform.position = absoluteMovement; //new Vector3(absoluteMovement.x, 0, absoluteMovement.z);
         previousChunk = currentChunk;
     }
+
+    // private void PositionChunk(RoadChunk first, RoadChunk second)
+    // {
+    //     var offset = first.start.position - second.end.position;
+    //     second.transform.Translate(offset);
+    // }
 
     private void UpdateChunk()
     {
         currentChunk = Instantiate(Resources.Load<RoadChunk>("Road/TestRoad/2"));
-        var absoluteMovement = previousChunk.end.position - currentChunk.start.position;
-        currentChunk.transform.position = absoluteMovement;
+        var absoluteMovement = previousChunk.end.position -  currentChunk.start.position;
+        if (previousChunk != null)
+        {
+            Debug.DrawLine(previousChunk.end.transform.position, previousChunk.start.transform.position, Color.cyan,
+                100);
+            Debug.DrawLine(currentChunk.end.transform.position, currentChunk.start.transform.position, Color.cyan, 100);
+            Debug.DrawLine(previousChunk.end.transform.position, currentChunk.start.transform.position, Color.red, 100);
+        }
+        currentChunk.transform.position += absoluteMovement;
         previousChunk = currentChunk;
     }
 }
