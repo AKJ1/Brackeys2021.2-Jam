df203ca18729139419ae8a5dd00673fe7e9c43c4
djfannywang@gmail <djfannywang@gmail.com>
2021-08-26 22:17:25 +0300

    Sudokkkku

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using PathCreation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
@@ -20,17 +21,39 @@ public class PlayerController : MonoBehaviour
 
     public float pastMagnitude;
 
+
+    [FormerlySerializedAs("StabilityLossRate")] public float stabilityLossFactor = 10;
+
+    public float currentStability = 0;
+    public float stabilityGainPerSecond = 1;
+    
+    [Range(0,1)]
+    public float minTraction = 0.02f;
+    public float maxTraction = 0.2f;
+    public float currentTraction;
+
+    public Quaternion previousRotation;
+
     private float motionDecay;
     private float movementMagnitude;
 
     void FixedUpdate()
     {
         var currentVelocity = rigidbody.velocity;
-        rigidbody.velocity =(currentVelocity * (1 - motionDecay)) +  playerForward.forward * movementMagnitude;
+
+        Quaternion.Angle(this.rigidbody.transform.rotation, previousRotation);
+
+        var newVelocity = (currentVelocity * (1 - motionDecay));
+        var reroutedVelocity = playerForward.forward * (newVelocity.magnitude * currentTraction);
+        var continuedVelocity = newVelocity * ( 1 - currentTraction);
+
+        var finalVelocity = reroutedVelocity + continuedVelocity;
+        rigidbody.velocity = finalVelocity + (playerForward.forward * movementMagnitude);
         // rigidbody.AddForce(playerForward.forward * (playerSpeed * input.y), ForceMode.Force);
         // rigidbody.AddTorque(rigidbody.transform.right * , ForceMode.Force);
 
         pastVelocity = rigidbody.velocity;
+        previousRotation = rigidbody.transform.rotation;
     }
 
     void Update()
@@ -38,6 +61,18 @@ public class PlayerController : MonoBehaviour
         var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         motionDecay = rigidbody.drag * Time.deltaTime;
         movementMagnitude = (playerSpeed * input.y * Time.fixedDeltaTime);
-        rigidbody.MoveRotation(rigidbody.transform.rotation * Quaternion.AngleAxis((playerRotationSpeed * input.x * Time.deltaTime), rigidbody.transform.up));
+        // rigidbody.angularVelocity = Vector3.forward * playerRotationSpeed *
+
+        var newRotation = rigidbody.transform.rotation *
+                          Quaternion.AngleAxis((playerRotationSpeed * input.x * Time.deltaTime),
+                              rigidbody.transform.up);
+        currentStability -= (1-Quaternion.Dot(rigidbody.transform.rotation, newRotation))* stabilityLossFactor;
+        currentStability += stabilityGainPerSecond * Time.deltaTime;
+        currentStability = Mathf.Clamp01(currentStability);
+        currentTraction = Mathf.Lerp(minTraction, maxTraction, currentStability);
+        rigidbody.MoveRotation(newRotation);
+        
+        
+        
     }
 }
