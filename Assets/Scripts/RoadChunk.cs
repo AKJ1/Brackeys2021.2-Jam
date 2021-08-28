d7f454e91e4119c4d37ea1fd0d6082d1513e2cd3
Katerina Koceva <k.kotseva@xs-software.com>
2021-08-28 21:44:50 +0300

    Added obstacles

--- Assets/Scripts/RoadChunk.cs
+++ Assets/Scripts/RoadChunk.cs
@@ -7,5 +7,76 @@ public class RoadChunk : MonoBehaviour
     public Transform start;
     public Transform end;
     public Transform roadChunk;
-    
+    public Transform transformRoad;
+    private MeshCollider roadMeshCollider;
+
+    public float startDistance = 10;
+    public float yDistance = 100;
+    public float minSpread = 5;
+    public float maxSpread = 10;
+    public int numPoints = 100;
+    private float boundingBoxScale = 1f;
+    private Vector3 pointRandom;
+    private Vector3 pointOnRoad = Vector3.zero;
+    private bool pointFound = false;
+    private int indexPoints = 0;
+    private RaycastHit hit;
+
+    void OnEnable()
+    {
+        roadMeshCollider = transformRoad.GetComponent<MeshCollider>();
+        GenerateRandomPositions();
+    }
+
+    void GenerateRandomPositions()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            if (indexPoints >= numPoints)
+            {
+                break;
+            }
+
+            pointRandom = new Vector3(
+            Random.Range(roadMeshCollider.bounds.min.x * boundingBoxScale, roadMeshCollider.bounds.max.x * boundingBoxScale),
+            Random.Range(roadMeshCollider.bounds.min.y * boundingBoxScale, roadMeshCollider.bounds.max.y * boundingBoxScale),
+            Random.Range(roadMeshCollider.bounds.min.z * boundingBoxScale, roadMeshCollider.bounds.max.z * boundingBoxScale));
+         
+            GetRandomPointOnColliderSurface(pointRandom, pointOnRoad);
+
+            if (pointFound)
+            {
+                indexPoints++;
+                //Transform cubeTransform = Instantiate(Resources.Load<Transform>("Obstacles/Cube"));
+                //cubeTransform.position = pointOnRoad;
+                //cubeTransform.localScale = new Vector3(2f, 2f, 2f);
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = pointOnRoad;
+                sphere.transform.localScale = new Vector3(2f, 2f, 2f);
+                sphere.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+            }
+        }
+    }
+
+    private void GetRandomPointOnColliderSurface(Vector3 point, Vector3 pointSurface)
+    {
+        pointOnRoad = Vector3.zero;
+        pointFound = false;
+      
+        if (Physics.Raycast(point - transformRoad.up, transformRoad.up, out hit, Mathf.Infinity))
+        {
+            pointOnRoad = hit.point;
+            pointFound = true;
+        }
+        else
+        {
+            if (Physics.Raycast(point + transformRoad.up, -transformRoad.up, out hit, Mathf.Infinity))
+            {
+                pointOnRoad = hit.point;
+                pointFound = true;
+            }
+        }
+
+        pointSurface = pointOnRoad;
+    }
 }
--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RoadGenerator : MonoBehaviour
 {
@@ -11,6 +12,21 @@ public class RoadGenerator : MonoBehaviour
     private RoadChunk previousChunk;
     private RoadChunk currentChunk;
     private List<RoadChunk> chunks = new List<RoadChunk>();
+    private MeshCollider roadMeshCollider;
+    [Tooltip("Maximal high of the surface.")]
+    public float surfaceHigh = 1f; //
+    [Tooltip("Number of random points to generate on the surface.")]
+    public int numPoints = 100;
+    [Tooltip("Maximal number of iterations to find the points.")]
+    public int maxIterations = 1000;
+    [Tooltip("Size of the generated sphere primitives.")]
+    public Vector3 scalePrimitives = new Vector3(2f, 2f, 2f);
+    [Tooltip("Color of the generated sphere primitives.")]
+    public Color colorPrimitives = Color.red;
+
+    private bool isUnityTerrain = true;
+    //private Collider m_collider;
+    private float bboxScale = 1f;
 
     private void Start ()
     {
@@ -58,7 +74,9 @@ public class RoadGenerator : MonoBehaviour
         var playerOffset = new Vector3(0,b.size.y*1.5f,0);
         
         currentChunk.transform.position += relativeMovement - playerOffset; //new Vector3(absoluteMovement.x, 0, absoluteMovement.z);
+        currentChunk.gameObject.SetActive(true);
         previousChunk = currentChunk;
+        previousChunk.gameObject.SetActive(true);
     }
 
     // private void PositionChunk(RoadChunk first, RoadChunk second)
@@ -70,7 +88,6 @@ public class RoadGenerator : MonoBehaviour
     private void UpdateChunk()
     {
         currentChunk = Instantiate(Resources.Load<RoadChunk>("Road/TestRoad/2"));
-        chunks.Add(currentChunk);
         var absoluteMovement = previousChunk.end.position -  currentChunk.start.position;
         // if (previousChunk != null)
         // {
@@ -81,8 +98,87 @@ public class RoadGenerator : MonoBehaviour
         // }
         
         currentChunk.transform.position += absoluteMovement;
+        //GenerateObstacles();
         previousChunk = currentChunk;
+        chunks.Add(previousChunk);
 
        
+        previousChunk.gameObject.SetActive(true);
+
     }
+
+    //public void GenerateObstacles()
+    //{
+    //    roadMeshCollider = currentChunk.prefabCollider.GetComponent<MeshCollider>();
+    //    GenerateRandomPositions();
+    //}
+
+    //void GenerateRandomPositions()
+    //{
+    //    Vector3 pointRandom;
+    //    Vector3 pointOnSurface = Vector3.zero;
+    //    bool pointFound = false;
+    //    int indexPoints = 0;
+    //    int indexLoops = 0;
+    //    do
+    //    {
+    //        indexLoops++;
+    //        // Double the size of the bounding box here to get better results if not a Unity terrain
+    //        if (isUnityTerrain) bboxScale = 1f;
+    //        else bboxScale = 2f;
+
+    //        if (isUnityTerrain) pointRandom = RandomPointInBounds(roadMeshCollider.bounds, bboxScale);
+    //        else pointRandom = RandomPointInBounds(roadMeshCollider.bounds, bboxScale) - transform.position;
+
+    //        pointFound = GetRandomPointOnColliderSurface(pointRandom, out pointOnSurface);
+
+    //        if (pointFound)
+    //        {
+    //            indexPoints++;
+    //            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    //            sphere.transform.position = pointOnSurface;
+    //            sphere.transform.localScale = scalePrimitives;
+    //            sphere.GetComponent<Renderer>().material.color = colorPrimitives;
+    //            //sphere.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+    //        }
+    //    } while ((indexPoints < numPoints) && (indexLoops < maxIterations));
+    //}
+
+    //private bool GetRandomPointOnColliderSurface(Vector3 point, out Vector3 pointSurface)
+    //{
+
+    //    Vector3 pointOnSurface = Vector3.zero;
+    //    RaycastHit hit;
+    //    bool pointFound = false;
+    //    // Raycast against the surface of the transform
+    //    Debug.DrawRay(point - surfaceHigh * transform.up, transform.up * surfaceHigh, Color.green, 5f);
+    //    if (Physics.Raycast(point - surfaceHigh * transform.up, transform.up, out hit, Mathf.Infinity))
+    //    {
+    //        //Debug.Log("Found point up");
+    //        pointOnSurface = hit.point;
+    //        pointFound = true;
+    //    }
+    //    else
+    //    {
+    //        Debug.DrawRay(point + surfaceHigh * transform.up, -transform.up * surfaceHigh, Color.red, 5f);
+    //        if (Physics.Raycast(point + surfaceHigh * transform.up, -transform.up, out hit, Mathf.Infinity))
+    //        {
+    //            //Debug.Log("Found point -up");
+    //            pointOnSurface = hit.point;
+    //            pointFound = true;
+    //        }
+    //    }
+
+    //    pointSurface = pointOnSurface;
+    //    return pointFound;
+    //}
+
+    //private Vector3 RandomPointInBounds(Bounds bounds, float scale)
+    //{
+    //    return new Vector3(
+    //        Random.Range(bounds.min.x * scale, bounds.max.x * scale),
+    //        Random.Range(bounds.min.y * scale, bounds.max.y * scale),
+    //        Random.Range(bounds.min.z * scale, bounds.max.z * scale)
+    //    );
+    //}
 }
