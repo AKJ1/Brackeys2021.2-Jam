98dd55f73b3ec01af06cd9cd464c304e48a1bc48
djfannywang@gmail <djfannywang@gmail.com>
2021-08-29 12:01:14 +0300

    Add speed increase when bumping into stuff

--- Assets/Scripts/Destructables/SwitchDestructable.cs
+++ Assets/Scripts/Destructables/SwitchDestructable.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -14,14 +15,22 @@ public class SwitchDestructable : MonoBehaviour
     public ParticleSystem CrashParticles;
     public Action onCollisionEnter;
 
+    private Collider[] hitObjects = new Collider[25];
+    private Vector3[] collisionPoints;
+
+    private float impactMagnitude;
+
     private void OnCollisionEnter(Collision col)
     {
         if (col.transform.CompareTag("Player"))
         {
-            GameController.instance.DecreasePlayerSpeed();
-
+            GameController.instance.IncreasePlayerSpeed();
+            impactMagnitude = col.impulse.magnitude;
+            
+            collisionPoints = col.contacts.Select(c => c.point).ToArray();
             foreach (var point in col.contacts)
             {
+                Coroutiner.StartCoroutine(KnockStuffDown());
                 var particles = Instantiate(CrashParticles.gameObject, point.point, quaternion.LookRotation(point.normal, Vector3.up));
                 var system =particles.GetComponent<ParticleSystem>();
                 system.Play();
@@ -29,6 +38,26 @@ public class SwitchDestructable : MonoBehaviour
             FracturedParent.gameObject.SetActive(true);
             WholeParent.gameObject.SetActive(false);
 
+        }
+    }
+
+    IEnumerator KnockStuffDown()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        foreach (var cp in collisionPoints)
+        {
+            Physics.OverlapSphereNonAlloc(cp, 1, this.hitObjects);
+            foreach (var obj in hitObjects)
+            {
+                if (obj != null)
+                {
+                    if (obj.attachedRigidbody != null)
+                    {
+                        obj.attachedRigidbody.AddExplosionForce(impactMagnitude, cp, 1);
+                    }
+                }
+            }
         }
     }
 
